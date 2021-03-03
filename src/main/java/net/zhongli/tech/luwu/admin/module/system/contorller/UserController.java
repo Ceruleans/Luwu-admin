b4bca16b7fdd54633968f690da2ee6dcc72f3625
958143409@qq.com <OZY941129>
2021-03-03 17:06:14 +0800

    修复table bug

--- src/main/java/net/zhongli/tech/luwu/admin/module/system/contorller/UserController.java
+++ src/main/java/net/zhongli/tech/luwu/admin/module/system/contorller/UserController.java
@@ -60,7 +60,7 @@ public class UserController extends BaseController {
     //@PreAuthorize("hasAuthority('system:users:list:post')")
     @PostMapping("/list")
     @ResponseBody
-    public Result<Pager> userList(@RequestBody Pager pager) {
+    public Pager userList(@RequestBody Pager pager) {
         Page<Object> page = PageHelper.startPage(pager.getPageNum(), pager.getPageSize());
         List<UserEntity> userEntityList = this.userService.queryList(pager.getParameters());
         return ResultUtil.page(page, userEntityList);
